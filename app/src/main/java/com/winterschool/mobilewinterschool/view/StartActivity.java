c94fd2d1dd730cee2df267565f9e3bd0aaf1847e
Bashev Vladislav <vlad-498@yandex.ru>
2017-02-07 09:59:58 +0700

    Update somethings
    ic_launcher AndroidManifest

--- app/src/main/java/com/winterschool/mobilewinterschool/view/StartActivity.java
+++ app/src/main/java/com/winterschool/mobilewinterschool/view/StartActivity.java
@@ -4,20 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 
+import com.winterschool.mobilewinterschool.LoginActivity;
 import com.winterschool.mobilewinterschool.R;
 
 /**
  * Created by vlad- on 06.02.2017.
  */
 
-public class StartActivity extends Activity implements View.OnClickListener {
+public class StartActivity extends Activity {
 
     private Handler mHandler;
     private static final int STOP_DELAY = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
@@ -29,12 +33,13 @@ public class StartActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View v) {
+    public boolean onTouchEvent(MotionEvent event) {
         stopActivity();
+        return super.onTouchEvent(event);
     }
 
     private void stopActivity(){
-        Intent intent = new Intent(this, SettingsActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
